b9ef45035ea995c2b473678a20130362b217df29
Philip Walton <philip@philipwalton.com>
2017-09-14 11:42:35 -0700

    Update the modern bundle's browserlist config

--- tasks/bundles.js
+++ tasks/bundles.js
@@ -94,12 +94,13 @@ const modernConfig = Object.assign({}, baseConfig, {
   module: {
     rules: [
       configureBabelLoader([
-        'last 2 Chrome versions',
-        'last 2 Safari versions',
-        'last 2 iOS versions',
-        'not Chrome < 61',
-        'not Safari < 10.1',
-        'not iOS < 10.3',
+        // The last two versions of each browser, excluding versions
+        // that don't support <script type="module">.
+        'last 2 Chrome versions', 'not Chrome < 60',
+        'last 2 Safari versions', 'not Safari < 10.1',
+        'last 2 iOS versions', 'not iOS < 10.3',
+        'last 2 Firefox versions', 'not Firefox < 54',
+        'last 2 Edge versions', 'not Edge < 15',
       ]),
     ],
   },
